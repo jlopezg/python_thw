f25b6d906fb5e8c929fc5f1acf971f5e4b822e1c
kelvin peña <kelvinpcorniel@gmail.com>
2019-02-08 11:10:55 -0400

    add ex2.py

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# A coment, this is so you can read your program later.
+# anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored 
+  
+#you can also use a coment to "disable" or coment out a piece of code:
+#print "this won' t run."
+  
+print "This will run."
+  
