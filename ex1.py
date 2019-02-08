734ec564e6f3ce293b458f0cb03580f1f8bca7ca
Jaison <yeison0317@windowslive.com>
2019-02-08 10:20:25 -0400

    ADDED ex1.py file

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rater you 'not'."
+print 'I "said" do not touch this.'
+print "iPhone Xs Max"
