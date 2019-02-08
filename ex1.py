86c183e6747aaad79b0c09abdf344f1ca811f8cb
Jose Lopez <joselopezg8@gmail.com>
2019-02-08 10:22:01 -0400

    added ex1.py file

---
+++ ex1.py
@@ -0,0 +1,8 @@
+-*- coding: utf-8 -*-
+print "hello world!"
+print "hello again"
+print "l like ttping this"
+print "this is fun" 
+print 'yay! printing'
+print "i'd much rather you 'not'. "
+print 'i "said" do not touch this'
