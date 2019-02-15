00722c1b292b4af0a164487b9e41f8ed7905949e
kelvin peña <kelvinpcorniel@gmail.com>
2019-02-15 12:07:18 -0400

    add ex8.py

---
+++ ex8.py
@@ -0,0 +1,14 @@
+fotmatter = "%r %r %r %r"
+
+print fotmatter % (1, 2, 3, 4)
+print fotmatter % ("one", "two", "theree", "four")
+print fotmatter % (True, False, False, True)
+print fotmatter % (fotmatter, fotmatter, fotmatter, fotmatter)
+print fotmatter % (
+    "I had this thing.",
+	"That you could type up right.",
+	"But it did't sing.",
+	"so I said goodnigt."
+)
+
+
