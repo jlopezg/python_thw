b2947751ecbd8e92d1c6660af8b122bc6df300b1
Jose Lopez <joselopezg8@gmail.com>
2019-02-15 12:04:49 -0400

    ex8.py

---
+++ ex8.py
@@ -0,0 +1,14 @@
+# -- coding: utf-8 --
+
+fomatter = "%r %r %r %r"
+
+print fomatter % (1,2,3,4)
+print fomatter % ("one", "two", "three", "four")
+print fomatter % ( True, False, False, True)
+print fomatter % (fomatter, fomatter, fomatter, fomatter)
+print fomatter % (
+
+	"I had this thing",
+	"that you could type up right",
+	"But it didn' t sing. ",
+	"So I said goodnight.")
