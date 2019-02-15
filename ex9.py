f4482cdfd0eab085f47d366cb8e6dd96c4af3690
Jose Lopez <joselopezg8@gmail.com>
2019-02-15 12:39:27 -0400

    ex9.py

---
+++ ex9.py
@@ -0,0 +1,15 @@
+# -- coding: utf-8 --
+
+# here' s some new strange stuff, remember typr it exactly.
+
+days =  "Mon tue wsd thu Fri sat Sun"
+months =  "\nJan\nFeb\nMar\nApr\nMay\nJun\nAug"
+
+print "Here re the days: ", days
+print "Here are the months: ",months
+print """
+there' s somthing going on here.
+with the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6."""
+
