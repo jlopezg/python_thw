c5b609c07df1e42ba7ee009291979f8d86176c16
kelvin peña <kelvinpcorniel@gmail.com>
2019-02-15 12:31:13 -0400

    add ex9.py

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exacctly.
+
+days = "Mon Tue wed Thu Fri Sat Sun"
+months = "jan\nFeb\nMar\nApr\nMary\nJun\nJul\rAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+with the theree double-quotes.
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
