7651537d60ad62b60555048a621428b2904a8e10
Jose Lopez <joselopezg8@gmail.com>
2019-02-22 10:59:07 -0400

    ex13.py

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 --
+
+from sys import argv
+
+script,first, second, third =argv
+
+print "The scrpt is called:", script
+print "Your first variable is:",first
+print "Your second varible is:",second
+print "your third varible is:", third
