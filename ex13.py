6576288435b4b2c8d382ce34d9b017fb04245c03
kelvin peña <kelvinpcorniel@gmail.com>
2019-02-22 11:01:14 -0400

    add ex13.py

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, secound, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your secound variable is:", secound
+print "Your third variable is:", third
