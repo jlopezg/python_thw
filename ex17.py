9bfcb516084fe75ddcd3adc7660651c4caa7a713
Jaison <yeison0317@windowslive.com>
2019-03-01 10:13:02 -0400

    [REF] added ex17.py file

--- ex17.py
+++ ex17.py
@@ -8,8 +8,8 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" %(from_file, to_file)
 
 # we could do these two on one line too, how?
-in_file = open(from_file)
-indata = in_file.read()
+in_file = open(from_file); indata = in_file.read()
+
 
 print "The input file is %d bytes long" % len(indata)
 
