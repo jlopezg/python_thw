6616f2b381ae072ecb2889347d58dfcc06e13c72
Jaison <yeison0317@windowslive.com>
2019-03-01 09:31:14 -0400

    [REF] added ex16.py file

--- ex16.py
+++ ex16.py
@@ -24,12 +24,12 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line1 + "\n" + line2 + "\n" + line3 + "\n" )
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
 
 print "And finally, we close it. "
 target.close()
