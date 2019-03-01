2b67470fad2c0b8e1082da6091df81abb691c30e
Jaison <yeison0317@windowslive.com>
2019-03-01 10:55:32 -0400

    [ADD] added ex18.py file

---
+++ ex18.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# this one is like yoyr scripts with argv
+def print_two(*args):
+	arg1, arg2, arg3 = args
+	print "arg1: %r, arg2: %r, arg3: %r" %(arg1, arg2, arg3)
+	
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# this just takes one argument
+def print_one(arg1):
+	print "arg1: %r" % arg1
+	
+# this one takes no arguments
+def print_none():
+	print "I got nothing'."
+	
+
+print_two("Zed","Shaw","Mendez")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none() 
