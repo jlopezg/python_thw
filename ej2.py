b2c2ffcb8d1e87b1ff72ba9fe4762ac05bb30dbf
Riky <47247430+Rikylora30@users.noreply.github.com>
2019-03-06 23:08:03 -0400

    ej2.py

---
+++ ej2.py
@@ -0,0 +1,14 @@
+# -- coding: utf-8 --
+numeros1  = 0
+numeros = int(input("Intruduzca un numero: "))
+
+
+
+while numeros != 0:
+
+	numeros1  =  numeros1 + numeros 
+	numeros = int(input("Intruduzca otro numero: "))
+	
+	
+	
+print "Esta es la suma de los Numeros: ",numeros1
